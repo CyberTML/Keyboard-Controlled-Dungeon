205d7384b930122e188288f992a096991cfb2975
CyberTML <cybertml@outlook.com>
2022-09-09 13:06:25 -0400

    Library Updated

---
+++ Main.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Keyboard_Controlled_Menu
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Game game = new Game();
+            game.Start();
+
+        }
+    }
+
+
+}
